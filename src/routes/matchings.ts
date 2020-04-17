0a9b4ccecd6b28983d68cbac1ad49de40840acdd
Clayderson Ferreira <contato@clayderson.com.br>
2020-04-17 16:09:12 -0300

    Adicionando paginacao a rota de listagem de matchings

--- src/routes/matchings.ts
+++ src/routes/matchings.ts
@@ -6,9 +6,35 @@ const logger = debug('challenge:matchings');
 
 export default (router: Router) => {
   router.get('/matchings', async (request: Request, response: Response) => {
+    let page = parseInt(
+      request.query.page ? request.query.page.toString() : '0', 10
+    );
+
+    let limit = parseInt(
+      request.query.limit ? request.query.limit.toString() : '0', 10
+    );
+
+    if (page < 1) {
+      page = 1;
+    }
+
+    if (limit < 1 || limit > 100) {
+      limit = 100;
+    }
+
+    const total = await Matching.estimatedDocumentCount();
+    const totalPages = Math.round(total / limit) >= 1 ? Math.round(total / limit) : 1;
+
+    const matchings = await Matching.find().populate([
+      'recording', 'tabulation'
+    ]).skip(limit * (page - 1)).limit(limit);
+
     try {
       response.json({
-        data: await Matching.find().populate(['recording', 'tabulation']),
+        total,
+        pages: totalPages,
+        currentPage: page,
+        data: matchings,
       });
     } catch (err) {
       logger(err);
